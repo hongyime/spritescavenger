11c98016567e1138b4a1cfdfcbb0db0c3d9f1a2a
b <hongyime@users.noreply.github.com>
2025-12-19 20:17:31 +0800

    llm

--- src/views/ExpeditionView.tsx
+++ src/views/ExpeditionView.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useGame } from "@/context/GameContext";
 import ExpeditionConsole from "@/components/ExpeditionConsole";
 import LootReveal from "@/components/LootReveal";
